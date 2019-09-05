d1416a054a3363d05768a62d755f54107e88ba18
Jryke <rykerson@gmail.com>
2019-09-05 14:29:39 +0700

    80202

--- airbnb-api/index.js
+++ airbnb-api/index.js
@@ -3,3 +3,6 @@ const express = require('express')
 const app = express()
 
 app.listen(4000, () => console.log('Ready on port 4000'))
+
+// 80202
+app.get('/', (req, res) => res.send('Welcome to Airbnb API'))
